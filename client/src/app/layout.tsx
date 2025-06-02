9782b6fd360acba7c2dbe93981db0eab4819de8f
Prabhu Patil <prabhumahesh.patil@gmail.com>
2025-06-02 23:34:07 +0530

    Initialize client

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Inter, JetBrains_Mono } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  variable: "--font-sans",
+  subsets: ["latin"],
+});
+
+const jetbrainsMono = JetBrains_Mono({
+  variable: "--font-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Webmind",
+  description: "IDK.",
+};
+
+const RootLayout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.variable} ${jetbrainsMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
+
+export default RootLayout
---
+++ client/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return (
+    <div>
+      <Button variant="ghost" className="bg-blue-700">Hey</Button>
+    </div>
+  );
+}
+
+export default Home
